4e1ac836ca7c71ffc93e1da871bc073521e5e634
Saad Inam <saadinam.mn@gmail.com>
2024-07-23 14:56:28 +0500

    Organising folders

---
+++ Views/Badges/BadgeBackground.swift
@@ -0,0 +1,47 @@
+//
+//  BadgeBackground.swift
+//  Landmarks
+//
+//  Created by Saad Inam on 15/07/2024.
+//
+
+import SwiftUI
+
+struct BadgeBackground: View {
+    var body: some View {
+        Path { path in
+            var width: CGFloat = 100.0
+            let height = width
+            path.move(
+                to: CGPoint (
+                    x: width * 0.95,
+                    y: height * 0.20
+                )
+            )
+            HexagonParameters.segments.forEach { segment in
+                path.addLine(
+                    to: CGPoint(
+                        x: width * segment.line.x,
+                        y: height * segment.line.y
+                    )
+                )
+                path.addQuadCurve(
+                                    to: CGPoint(
+                                        x: width * segment.curve.x,
+                                        y: height * segment.curve.y
+                                    ),
+                                    control: CGPoint(
+                                        x: width * segment.control.x,
+                                        y: height * segment.control.y
+                                    )
+                                )
+            }
+            
+        }
+        .fill(.black)
+    }
+}
+
+#Preview {
+    BadgeBackground()
+}
